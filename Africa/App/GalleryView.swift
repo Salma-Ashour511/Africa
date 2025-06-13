c8ca15043df24fad11e1762f9252d80816c7f51c
Salma.Ashour <salmaashour93@gmail.com>
2025-06-13 18:16:24 +0300

    Implement GalleryView functionality

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    let animals: [Animal] = Bundle.main.decode(file: "animals.json")
+    @State private var selectedAnimal = "lion"
+    
+    // Simple Grid definition.
+    //    @State private var gridLayout: [GridItem] = [
+    //        GridItem(.flexible()),
+    //        GridItem(.flexible()),
+    //        GridItem(.flexible())
+    //    ]
+    
+    // Efficient Grid definition.
+    //    @State private var gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // Dynamic Grid Layout.
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
+    }
+    
+    // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { _ in
+                        gridSwitch()
+                    }
+                    
+
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(lineWidth: 2))
+                            .onTapGesture {
+                                selectedAnimal = animal.image
+                            }
+                    }
+                }
+                .animation(.easeIn, value: 1.0)
+                .onAppear {
+                    gridSwitch()
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
             
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -17,6 +74,7 @@ struct GalleryView: View {
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     GalleryView()
 }
